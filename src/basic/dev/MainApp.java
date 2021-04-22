6a2dc54967d5b0a9aa935e6ee4241d7f3915bd73
duciker14 <duciker14@gmail.com>
2021-04-22 13:32:56 +0700

    HelloWorld

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,12 @@
+package basic.dev;
+/*
+ * Tao project dau tien: HelloWorld
+ */
+public class MainApp {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
